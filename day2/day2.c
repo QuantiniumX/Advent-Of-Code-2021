4087e2c05452b0e6f2b728e13d272ac99db2d8d4
QuantiniumX <vaibhavvx331@gmail.com>
2023-10-29 23:11:56 +0530

    just need to add a zero

--- day2/day2.c
+++ day2/day2.c
@@ -1,5 +1,5 @@
-#include "stdio.h"
-#include "string.h"
+#include <stdio.h>
+#include <string.h>
 
 int main() {
     char line[100];
@@ -10,18 +10,18 @@ int main() {
     FILE *fp = fopen("./test.txt", "r");    
     while(fgets(line, sizeof(line), fp) != NULL){
         if(sscanf(line, "%19s %d", action, &value) == 2){
-            if(strcmp(action, "forward")){
+            if(strcmp(action, "forward") == 0){
                 forward += value;
-                printf("%d\n %d\n", forward, value);
             }
-            else if(strcmp(action, "down")){
-                depth -= value;
-            }
-            else if(strcmp(action, "up")){
+            else if(strcmp(action, "down") == 0){
                 depth += value;
             }
-     }
+            else if(strcmp(action, "up") == 0){
+                depth -= value;
+            }
+        }
     }
     fclose(fp);
-    printf("forward: %d\n depth: %d\n", forward, depth);
+    printf("Product of final horizontal position and final depth is: %d\n", forward * depth);
 }
+
